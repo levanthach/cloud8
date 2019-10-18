935c741edf8909e643623fbffc184c6a9bcad46a
lethach <lethach150699@gmail.com>
2019-10-18 10:58:12 +0700

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -188,9 +188,9 @@ jQuery(function($){
   
   // for hover dropdown menu
     jQuery('ul.nav li.dropdown').hover(function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+      jQuery(this).find('.dropdown-menu').stop(true, true).fadeIn(200);
     }, function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+      jQuery(this).find('.dropdown-menu').stop(true, true).fadeOut(200);
     });
 
     
